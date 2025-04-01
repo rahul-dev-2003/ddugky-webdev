97b43a951f1ca49470ab790a7e8080203b587f3e
Rahul Garwal <rahul.garwal.2003@gmail.com>
2025-04-01 11:11:19 +0530

    Data types and Variables

---
+++ JS-Fundamentals/index.js
@@ -0,0 +1,30 @@
+// 1. Data types
+
+/////////////////////////////
+
+// 1. String , "Rahul", "true"
+
+// 2. Number ,26
+// 3. Bigest
+// 4. Boolean , true / false
+// 5. Undefine
+// 6. Null
+// 7. Symbol
+
+/////////////////////////////////////////////////////////
+
+// 02. Variable
+
+//////////////////////////////////////////////////
+
+var fName = "Rahul";
+var lName = "garwal";
+var age = 21;
+var marred = false;
+var wifeName;// undefine
+var thirdName = null;
+
+
+
+console.log("good morning",fName,lName,"!");
+console.log("hi my nname is ",fName,"and my age is",age,".");
