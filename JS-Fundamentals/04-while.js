39411e6e46454f1482249cf47edb8af74a345352
Rahul Garwal <rahul.garwal.2003@gmail.com>
2025-04-09 11:32:19 +0530

    Learn while Loop

---
+++ JS-Fundamentals/04-while.js
@@ -0,0 +1,25 @@
+let num = 1
+while(num<=10){
+    console.log(num,"=hello world")
+    num++
+}
+console.log("loop end")
+
+console.log("____________________________________-____-__________")
+
+
+num = 1;
+
+// 3 * 2 = 6
+
+while(num<=10){
+    console.log("3 *", num, "=", 3*num)
+    num++
+}
+
+console.log("____________________________________-____-__________");
+num = 1;
+while(num<=10){
+    console.log("2 *",num,  "=",num*num)
+    num++
+}
